9132ea3d7c3e8927eaf4b023863a2fe69577c2aa
hamid-shahmohammadi <hamid.shahmohamadi@gmail.com>
2023-12-30 01:23:50 +0330

    7.for

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 function component1() {
     return {
-        init() {
-            console.log('hi');
-            this.myInput = 'something2'
-        },
-        keyUpVal($event){
-            this.myInput=$event.target.value
-        },
-        color:'red',
-        isShow: false,
-        myInput: 'something',
-        isDisabled: false,
-        myStyle: 'color:red',
-        myClass: 'bg-blue-400 p-4 text-white w-1/2 mx mx-auto text-center'
+        todos:[],
+        async init() {
+            self=this;
+            await fetch('https://jsonplaceholder.typicode.com/todos')
+            .then(response => response.json())
+            .then(json => self.todos=json)
+            console.log(self.todos)
+        },           
     }
 }
